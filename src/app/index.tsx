d616ded022786734d4256a60db5de95efd7a6a98
LUCAS FELIPE MARCELINO DA SILVA <lucas.fmsilva10@senacsp.edu.br>
2024-08-01 21:52:41 -0300

    iniciando projeto

---
+++ src/app/index.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native'
+
+
+export default function Home(){
+    return(
+        <View>
+            <Text className='text-black font-medium '>
+                    Olá 
+            </Text>
+        </View>
+    )
+}
