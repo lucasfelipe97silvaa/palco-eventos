a2b03b83adce7ed15d17f37e05a782231b01684f
MATHEUS DONIZETI FERREIRA FRANCO <matheus.dffranco@senacsp.edu.br>
2024-08-19 22:04:14 -0300

    criado tipos das tabelas do banco de dados

---
+++ src/types/databaseTypes.ts
@@ -0,0 +1,41 @@
+interface IIngresso {
+    nome: string;
+    preco: number;
+}
+
+export interface IEvento {
+    id: string;
+    categoria_id: number;
+    nome: string;
+    data: Date;
+    horaInicio: string;
+    horaFim: string;
+    endereco: string;
+    descricao: string;
+    imagem: string;
+    ingressos: IIngresso[];
+}
+
+export interface ICategoria {
+    id: number;
+    nome: string;
+}
+
+interface IIngressoPedido {
+    nome: string;
+    preco: string;
+    quantidade: number
+}
+
+export interface IPedido {
+    id: string;
+    usuario_id: string;
+    evento_id: string;
+    ingressos: IIngressoPedido[];
+}
+
+export interface IFavorito {
+    id: string;
+    evento_id: string;
+    usuario_id: string;
+}
